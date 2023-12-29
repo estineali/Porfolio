f539475538948eded85e0320c959d95f5079a7bc
Shahrom <mshahromali@gmail.com>
2023-12-29 04:04:14 -0500

    remove firebase

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-};
-
-// Initialize Firebase
-export default firebaseConfig;
