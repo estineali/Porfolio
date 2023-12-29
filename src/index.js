d1ba93f8b09755df87b6cd3c408f8dd23f6345f4
Shahrom <mshahromali@gmail.com>
2023-12-29 04:25:35 -0500

    Fix page scalable height issue

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
         style={{
           backgroundColor: "#131515",
           width: "100%",
-          height: "100%",
+          minHeight: "100%",
           position: "absolute",
         }}
       >
